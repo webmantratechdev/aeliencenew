8bb0e0bc7d0db3bae5c21739c69c70163c6730d3
webmantratechdev <dheeraj.webmantra@gmail.com>
2023-01-30 16:41:08 +0530

    commission

---
+++ resources/js/components/fronted/commission.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <section class="commissionBannerSec" style="background-image:url(images/bg11.jpg);">
+	<div class="container">
+	<div class="commissionBannerSecinner">
+	<div class="commissionBannerContent">
+	<h1>Invite Friends and Earn Crypto Together</h1>
+	<h5 class="para">Up to <span class="bigFont">40%</span> Trading Rebate  Referral Rules </h5>
+	<div class="commissionBannerBtnArea">
+	<a href="#" class="commissionBannerBtn">Invite Now</a>
+	</div>
+	</div>
+	
+	</div>	
+	</div>
+	</section>
+    
+    <section class="howReferralWorkSec">
+	<div class="container">
+	<div class="themeHeader text-center mb-5">
+	<h2 class="title mb-0">How Referral Works</h2>
+	</div>
+	<div class="howReferralWorkSecinner">	
+	
+	<div class="row rowBox">
+	
+	<div class="col-lg-4 col-md-4 col-md-6 col-12 columnBox howReferralWorkBox">
+	<div class="howReferralWorkBoxinner">
+	<div class="icon">
+	<img src="images/commission11.png" class="img-fluid" alt="">
+	</div>
+	<div class="content">
+	<h4>Share Commission</h4>
+	<p>Set how much referral commission you want to share with your friends</p>
+	</div>
+	</div>
+	</div>
+	
+	<div class="col-lg-4 col-md-4 col-md-6 col-12 columnBox howReferralWorkBox">
+	<div class="howReferralWorkBoxinner">
+	<div class="icon">
+	<img src="images/refer11.png" class="img-fluid" alt="">
+	</div>
+	<div class="content">
+	<h4>Refer Friends</h4>
+	<p>Share your referral link or QR code with your friends and social media</p>
+	</div>
+	</div>
+	</div>
+	
+	<div class="col-lg-4 col-md-4 col-md-6 col-12 columnBox howReferralWorkBox">
+	<div class="howReferralWorkBoxinner">
+	<div class="icon">
+	<img src="images/income11.png" class="img-fluid" alt="">
+	</div>
+	<div class="content">
+	<h4>Earn Crypto</h4>
+	<p>Earn up to 40% commission when your friends start trading</p>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	
+	</div>	
+	</section>
+	
+	<section class="startEarningNowSec">
+	<div class="container">
+	<div class="startEarningNowSecinner">
+	<div class="startEarningNowContent text-center">
+	<h2>Start Earning Now</h2>
+	<a href="#" class="loginLink">Login In</a>
+	</div>
+	</div>
+	
+	</div>	
+	</section>
+
+   
+
+    </div>
+</template>
---
+++ resources/js/components/fronted/contact.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        
+	<section class="crypto-portfolio contactpage_sec">
+        <div class="contactpage_sec_container">
+		<div class="text-center py-5">
+            <h2><span class="partner"></span> <span class="crypto">Contctact</span> <span class="partner">Us</span></h2>
+            <p>A free consultation is available to discuss your business needs, no obligation,<br>just a friendly chat to work out if we can help.</p>
+        </div>
+		 <div class="contactpage_secinner">
+		  <div class="row row_box" style="align-items: center;justify-content: center;">
+		  
+			<div class="col-lg-6 col-md-6 col-sm-12 col-12 column_box">
+			<div class="contactpage_form_area">
+			<div class="contactpage_form_areainner">
+			<form>
+			<div class="row row_box">
+			
+			<div class="col-lg-6 col-md-6 col-sm-12 col-12 column_box">
+			<div class="form-group">
+			<label>Name</label>
+			<input type="text" class="form-control" required="">
+			</div>
+			</div>
+			
+			<div class="col-lg-6 col-md-6 col-sm-12 col-12 column_box">
+			<div class="form-group">
+			<label>Email</label>
+			<input type="email" class="form-control" required="">
+			</div>
+			</div>
+			
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12 column_box">
+			<div class="form-group">
+			<label>Phone No.</label>
+			<input type="number" class="form-control" required="">
+			</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12 column_box">
+			<div class="form-group">
+			<label>Subject</label>
+			<input type="text" class="form-control" required="">
+			</div>
+			</div>
+			
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12 column_box">
+			<div class="form-group">
+			<label>Message</label>
+			<textarea class="form-control" required=""></textarea>
+			</div>
+			</div>
+			
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12 column_box">
+			<div class="contact_btn_area">			
+			<button class="btn btn-exchange contact_btn">Send Message</button>
+			</div>
+			</div>
+			
+			</div>
+			</form>
+			</div>
+			</div>
+			</div>
+			
+			<!--<div class="col-lg-6 col-md-6 col-sm-12 col-12 column_box">
+			<div class="contactpage_info_area">
+			
+			<div class="contactpage_info_blk">
+			<div class="contact_address_area44_block">
+			<div class="contact_address_area44_blockinner">
+			<div class="cuntry_img_area">
+			<img src="images/address_img1.jpg" class="img-fluid">
+			</div>
+			<div class="contact_address_area44_content">
+    			<h3>Aeliance Technologies EST</h3>   
+				<h4>Dubai, United Arab Emirates</h4>	
+    			<p><i class="fi fi-rr-marker"></i> Address: 16th Floor, Office No. 1601/1602, The Regal Tower, Business Bay, Dubai (UAE), 500001</p>
+				<p><i class="fi fi-rr-envelope"></i> Email: info@aelince.com</p>
+    			<a href="https://goo.gl/maps/hA87KedkLQj44Ab26" target="_blank" class="map"><span class="icon"><i class="fi fi-rr-map-marker"></i></span> <span class="txt">Google Maps</span></a>
+			</div>
+			</div>
+			</div>
+			</div>
+			
+			
+			<div class="contactpage_info_blk">
+			<div class="contact_address_area44_block">
+			<div class="contact_address_area44_blockinner">
+			<div class="cuntry_img_area">
+			<img src="images/address_img2.jpg" class="img-fluid">
+			</div>
+			<div class="contact_address_area44_content">
+    			<h3>Development Center</h3>
+    			<h4>Kolkata, India</h4>
+    			<p><i class="fi fi-rr-marker"></i> Address: Godrej Genesis Building - Level 11 Block EP &amp; GP, opp. to Syndicate Bank, Sector V, Kolkata, West Bengal 700091</p>
+				<p><i class="fi fi-rr-envelope"></i> Email: business@aelince.com</p>
+    			<a href="https://goo.gl/maps/B7eABkhCmNwvamZu8" target="_blank" class="map"><span class="icon"><i class="fi fi-rr-map-marker"></i></span> <span class="txt">Google Maps</span></a>
+			</div>
+			</div>
+			</div>
+			</div>
+			
+			
+			</div>
+			</div>-->
+			</div>
+		  </div>
+		 </div>
+		</div>
+    </section>
+    </div>
+</template>
