ea8bc13588f72348db54ac76304d9bd57e70d4b6
webmantratechdev <dheeraj.webmantra@gmail.com>
2023-01-30 16:13:35 +0530

    about

--- resources/js/components/fronted/About.vue
+++ resources/js/components/fronted/About.vue
@@ -1,3 +1,85 @@
 <template>
-    <div></div>
+    <div>
+    <section class="Trusted-platform py-5 overflow-hidden">
+        <div class="d-flex flex-column flex-lg-row flex-md-column justify-content-between align-items-center">
+            <div data-aos="fade-right" data-aos-duration="3000" class="lg-w-50 md-w-100 w-100 px-5 justify-content-center aos-init aos-animate">
+                <img class="img-fluid" src="./images/anydevice.png" alt="">
+            </div>
+            <div class="lg-w-50 md-w-100 w-100 px-5">
+                <h2><span class="partner">Trusted</span> <span class="platform">platform</span></h2>
+                <h2 class="partner">anytime &amp; anywhere</h2>
+                
+                <p class="mt-3">This is a unites and secures a growing exosystem of specialized blockchains called
+                    parachains. Apps
+                    and services on Polkadot can ecosystem of specialized called.Polkadot unites and secures a growing ecosystem of specialized
+                    blockchains called parachains. Apps and services.
+                    <br>
+                    <br>
+                    Polkadot unites and secures a growing ecosystem of specialized
+                    blockchains called parachains. Apps and services.Polkadot unites and secures a growing ecosystem of specialized
+                    blockchains called parachains. Apps and services.
+                </p>
+
+                
+            </div>
+        </div>
+    </section>
+	
+	
+	<section class="what-new overflow-hidden">
+        <div class=" d-flex flex-column flex-lg-row flex-md-column align-items-center pt-5">
+            <div class="w-100 lg-w-50 p-5 text-lg-start text-center">
+                <h2><span class="partner fw-bold">What’s</span> <span class="crypto fw-bold">new</span> <span class="partner fw-bold">we</span>
+                </h2>
+                <h2 class="fw-bold partner">Provide for you?</h2>
+                <p class="border-left">This is a unites and secures a growing ecosystem of specialized <br>
+                    blockchains called parachains. Apps and services on it.</p>
+
+                <div class="d-flex flex-column flex-lg-row justify-content-between mt-5 gap-2">
+                    <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center text-lg-start text-center py-2 w-100">
+                        <img class=" me-2" src="./images/lockt.png" alt="">
+                        <div class="text-content">
+                            <h6 class="text-white mt-2">Advanced Security</h6>
+                            <p class="text-center text-lg-start">We use state-of-the-art storage
+
+                                technology to protect your asse
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center text-lg-start text-center py-2 w-100">
+                        <img class=" me-2" src="./images/24.png" alt="">
+                        <div class="text-content">
+                            <h6 class="text-white mt-2">24/7 Customer Service</h6>
+                            <p class="text-center text-lg-start">Contact customer support with your questions at any
+                                time
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-column flex-lg-row justify-content-between gap-2">
+                    <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center text-lg-start text-center py-2 w-100">
+                        <img class=" me-2" src="./images/arabic.png" alt="">
+                        <div class="text-content">
+                            <h6 class="text-white text-center text-lg-start mt-2">Aelince news</h6>
+                            <p class="text-center text-lg-start">Keeping you up to date with the latest news in crypto
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center text-lg-start text-center py-2 w-100">
+                        <img class=" me-2" src="./images/communication.png" alt="">
+                        <div class="text-content">
+                            <h6 class="text-white text-center text-lg-start mt-2">Community</h6>
+                            <p class="text-center text-lg-start">The aelince community is home to millions of users from
+                                200+ countries, with support for 20+ languages.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-aos="fade-left" data-aos-duration="3000" class="w-100 lg-w-50 lg-mt-5 pt-0 pt-lg-5 overflow-hidden aos-init aos-animate">
+                <img class="img-fluid pt-0 lg-pt-5 ps-0 lg-ps-5 " src="./images/Laptop.png" alt="">
+            </div>
+        </div>
+    </section>
+    </div>
 </template>
