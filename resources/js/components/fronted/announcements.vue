9a484141146077568503e4ce30382949975c98cb
webmantratechdev <dheeraj.webmantra@gmail.com>
2023-01-30 16:22:20 +0530

    announcements

---
+++ resources/js/components/fronted/announcements.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <section class="sample_page_sec">
+        <div class="sample_page_secinner" style="min-height:600px;">
+		
+		 <div class="sample_page_content">
+		  <h2>AEL Carnival: Win AEL Token as Rewards by Referring Friends</h2>
+		  <p>Welcome to AEL Giveaway Airdrop</p>
+		  <p>You can get 10 AEL by joining our Airdrop.</p>
+		  <p>Earn 5 AEL Extra Rewards With The Confirmed Referrals.</p>
+		  <p>Airdrop rewards will be distributed to your wallet address on November 5th 2022.</p>
+		  <p>Participant in Giveaway <a href="https://t.me/aelcoin_giveaway_bot" style="color:#fec00f; text-decoration:none; font-weight:bold;" target="_blank">Telegram Bot</a></p>
+		  
+		  </div>
+		 
+		 </div>
+		
+		</div>
+    </section>  
+    </div>
+</template>
