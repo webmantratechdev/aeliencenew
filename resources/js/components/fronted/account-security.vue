212fe63a854c13328083202f7306fb02ccfc59b2
webmantratechdev <dheeraj.webmantra@gmail.com>
2023-01-30 16:16:37 +0530

    account-security

---
+++ resources/js/components/fronted/account-security.vue
@@ -0,0 +1,221 @@
+<template>
+<div>
+    
+	<section class="themeDashboardSec">
+	<div class="themeDashboardMenuHeader">
+	<div class="d-flex justify-content-between themeDashboardMenuHeaderinner">
+	<h4 class="title">Side Menu</h4>
+	<span class="sideMenu_btn">
+	<i class="fa-regular fa-bars"></i>	
+	</span>
+	</div>	
+	</div>
+	<div class="themeDashboardSecinner">
+	<div class="menuOverlay"></div>
+	<span class="menuCloseBtn">
+	<i class="fa-regular fa-xmark"></i>
+	</span>
+	<div class="themeDashboarSidebar">
+		<div class=""></div>
+		<ul class="navbar-nav">
+		<li class="nav-item">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-regular fa-wallet"></i></span>
+		<span class="txt">Overview</span>
+		</a>
+		</li>
+		<li class="nav-item">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-regular fa-flower"></i></span>
+		<span class="txt">Spot</span>
+		</a>
+		</li>
+		<li class="nav-item">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-light fa-list-dropdown"></i></span>
+		<span class="txt">Futures</span>
+		</a>
+		</li>
+		<li class="nav-item">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-light fa-calendar-clock"></i></span>
+		<span class="txt">Wallet History</span>
+		</a>
+		</li>
+		<li class="nav-item">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-regular fa-user"></i></span>
+		<span class="txt">Profile Information</span>
+		</a>
+		</li>
+		<li class="nav-item">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-light fa-address-card"></i></span>
+		<span class="txt">ID-Verification</span>
+		</a>
+		</li>
+		<li class="nav-item active">
+		<a href="#" class="nav-link">
+		<span class="icon"><i class="fa-light fa-file-invoice"></i></span>
+		<span class="txt">Account Security</span>
+		</a>
+		</li>
+		</ul>
+	</div>
+	<div class="themeDashboarMainArea">
+	<div class="container-fluid">
+	<div class="themeDashboarMainAreainner">
+		<div class="dashPanel">
+		<div class="dashPanelHeader">
+		<h2 class="title" style="margin-bottom:5px;">Account Security</h2>
+		<div class="d-flex align-items-center">
+		<div class="icon" style=""><img src="images/icon23.png" class="img-fuid icon" alt="" style="width: 28px;height: 28px;border-radius: 50%;margin: 0 10px 0 5px;"></div>
+		<div class="content"><h6 class="" style="margin-bottom:0px;">2FA Authentication.</div>
+		</div>
+		</div>
+		<div class="dashPanelBody accountSecurityFeature">
+		<div class="accountSecurityFeatureItem" style="">
+			<h4>2FA Authentication</h4>	
+
+			
+			<div class="accountSecurityFeatureListItem">
+			<div class="d-flex justify-content-between align-items-center">
+			<div class="d-flex leftSide">
+			<div class="icon">
+			<img src="images/icon23.png" class="img-fuid icon" alt="" style="width: 28px;height: 28px;border-radius: 50%;margin: 0 10px 0 5px;">
+			</div>
+			<div class="content">
+			<h5>SuperEx/Google/Microsoft Authentication</h5>
+			<p>Protect yor transaction and account security</p>
+			</div>
+			</div>
+			<div class="middleSide">
+			<p>Not Yet</p>
+			</div>
+			<div class="rightSide justify-content-between align-items-center btnLiast_area">
+			<a href="#" class="defaultBtn">Bind</a>			
+			</div>
+			</div>
+			</div>
+			
+			<div class="accountSecurityFeatureListItem">
+			<div class="d-flex justify-content-between align-items-center">
+			<div class="d-flex leftSide">
+			<div class="icon">
+			<img src="images/icon23.png" class="img-fuid icon" alt="" style="width: 28px;height: 28px;border-radius: 50%;margin: 0 10px 0 5px;">
+			</div>
+			<div class="content">
+			<h5>SuperEx/Google/Microsoft Authentication</h5>
+			<p>Protect yor transaction and account security</p>
+			</div>
+			</div>
+			<div class="middleSide">
+			<p>Not Yet</p>
+			</div>
+			<div class="rightSide justify-content-between align-items-center btnLiast_area">
+			<a href="#" class="defaultBtn">Change</a>
+			<a href="#" class="defaultBtn">Unbind</a>
+			</div>
+			</div>
+			</div>
+			
+			<div class="accountSecurityFeatureListItem">
+			<div class="d-flex justify-content-between align-items-center">
+			<div class="d-flex leftSide">
+			<div class="icon">
+			<img src="images/icon23.png" class="img-fuid icon" alt="" style="width: 28px;height: 28px;border-radius: 50%;margin: 0 10px 0 5px;">
+			</div>
+			<div class="content">
+			<h5>SuperEx/Google/Microsoft Authentication</h5>
+			<p>Protect yor transaction and account security</p>
+			</div>
+			</div>
+			<div class="middleSide">
+			<p>BTS*s ***@gmail.com</p>
+			</div>
+			<div class="rightSide justify-content-between align-items-center btnLiast_area">
+			<a href="#" class="defaultBtn">Change</a>			
+			</div>
+			</div>
+			</div>
+			
+			
+			
+			</div>	
+
+
+
+			<div class="accountSecurityFeatureItem" style="">
+			<h4>Advanced security</h4>	
+
+			
+			<div class="accountSecurityFeatureListItem">
+			<div class="d-flex justify-content-between align-items-center">
+			<div class="d-flex leftSide">
+			<div class="icon">
+			<img src="images/icon23.png" class="img-fuid icon" alt="" style="width: 28px;height: 28px;border-radius: 50%;margin: 0 10px 0 5px;">
+			</div>
+			<div class="content">
+			<h5>Login in Password</h5>
+			<p>Protect yor transaction and account security</p>
+			</div>
+			</div>
+			
+			<div class="rightSide justify-content-between align-items-center btnLiast_area">
+			<a href="#" class="defaultBtn">Change</a>			
+			</div>
+			</div>
+			</div>
+			
+			
+			
+			
+			
+			</div>	
+			
+			<div class="accountSecurityFeatureItem" style="">
+			<h4>Account security</h4>	
+
+			
+			<div class="accountSecurityFeatureListItem">
+			<div class="d-flex justify-content-between align-items-center">
+			<div class="d-flex leftSide">
+			<div class="icon">
+			<img src="images/icon23.png" class="img-fuid icon" alt="" style="width: 28px;height: 28px;border-radius: 50%;margin: 0 10px 0 5px;">
+			</div>
+			<div class="content">
+			<h5>Account activity</h5>
+			<p>Recent login : 2023-01-25 09:11:06</p>
+			</div>
+			</div>
+			
+			<div class="rightSide justify-content-between align-items-center btnLiast_area">
+			<a href="#" class="defaultBtn">More</a>			
+			</div>
+			</div>
+			</div>
+			
+			
+			
+			
+			
+			</div>	
+
+
+
+
+			
+		</div>
+		</div>	
+		
+		
+		
+		
+		
+	</div>	
+	</div>	
+	</div>	
+	</div>
+	</section>
+</div>
+</template>
